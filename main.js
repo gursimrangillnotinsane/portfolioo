011bb19f5ec1f6e2e3c566a654ca0d3f730cea11
Gursimran <gillsimranjot91@gmail.com>
2024-04-29 16:09:04 -0400

    Re-styled

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ function getRandomInt(min, max) {
 
 
 var root = document.documentElement;
-var listOfColors = ["#b97ca4cf", "#7c9cb9cf", "#547142a8", "#925c5e7f", "#424a3da8", "#3b854491", "#793b8591"]
+var listOfColors = ["#650000", "#066839", "#9C5B0C", "#2F9890"]
 var randomInteger = getRandomInt(0, 3);
 root.style.setProperty('--fg', listOfColors[randomInteger]);
+
+
