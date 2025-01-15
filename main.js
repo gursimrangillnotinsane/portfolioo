f11bb88c1a7328db6afcba40c6d0b13a10223d22
gursimrangillnotinsane <132660017+gursimrangillnotinsane@users.noreply.github.com>
2025-01-15 17:33:48 -0500

    updated

--- main.js
+++ main.js
@@ -15,22 +15,4 @@ root.style.setProperty('--fg', listOfColors[randomInteger]);
 const ticker = document.querySelector('.ticker')
 const list = document.querySelector('.ticker__list')
 
-let gg = false
-// ANIMATION WILL PLAY FOR 2S AND THEN FREEZE
-// TO DEMONSTRATE THE PROBLEM
-// setTimeout(() => {
-//     gg = true
-//     while (gg) {
-//         // event loop is blocked
-//         // animation freezes if translateX is a %
-//         // animation continues if translateX is set to pixels
-//         console.log("hi")
-//     }
-// }, 2000)
 
-
-
-// setTimeout(() => {
-//     gg = false
-
-// }, 3000)
